761eba9360a18b78f70802582240af87a6b7017f
mgumiero9 <mgumiero9@gmail.com>
2016-02-19 00:02:49 -0200

    Added Random Generator to view random messages

---
+++ app/src/main/java/com/androiders/funfacts/FunFactsActivity.java
@@ -0,0 +1,53 @@
+package com.androiders.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class FunFactsActivity extends AppCompatActivity {
+    // Declaring Variables
+
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Assigning TextView and Button to our variables
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+
+        // Defining the Listener object
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built." };
+
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(facts.length);
+
+                String fact = facts[randomNumber];
+                mFactTextView.setText(fact);
+
+            }
+        };
+
+        mShowFactButton.setOnClickListener(listener);
+    }
+}
