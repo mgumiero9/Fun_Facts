3b313bb46ecf40dd37e9e4bc48a33a43cfb21688
mgumiero9 <mgumiero9@gmail.com>
2016-02-20 01:16:27 -0200

    Toast Commented and Log.D Created to test

--- app/src/main/java/com/androiders/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/androiders/funfacts/FunFactsActivity.java
@@ -3,6 +3,7 @@ package com.androiders.funfacts;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -12,7 +13,7 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
-
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
     private FactBook mFactBook = new FactBook();
     private ColorWheel mColorWheel = new ColorWheel();
 
@@ -46,7 +47,9 @@ public class FunFactsActivity extends AppCompatActivity {
 
         mShowFactButton.setOnClickListener(listener);
 
-        Toast.makeText(FunFactsActivity.this, "Yeah! Our Activity is created!", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(FunFactsActivity.this, "Yeah! Our Activity is created!", Toast.LENGTH_SHORT).show();
+
+        Log.d(TAG, "Yeah! Our Activity is created!");
 
         
     }
